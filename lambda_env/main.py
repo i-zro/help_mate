f953eefe8b72165f5f5c4a64a0000f3f8581526b
i-zro <naa0@lguplus.co.kr>
2024-04-16 17:38:01 +0900

    message

--- lambda_env/main.py
+++ lambda_env/main.py
@@ -35,39 +35,49 @@ def lambda_handler(event, context):
             'statusCode': 200,
             'body': json.dumps({'message': 'Ignored bot message'})
         }
+    else:
+        # 스레드 메시지 중 최상위 메시지만 처리
+        event_ts = body['event'].get('ts')
+        thread_ts = body['event'].get('thread_ts', event_ts)
+    
+        if thread_ts != event_ts:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'message': 'Ignored sub-thread message'})
+            }
+            
+        channel_id = body['event']['channel']
+        thread_ts = body['event']['ts']
 
-    channel_id = body['event']['channel']
-    thread_ts = body['event']['ts']
+        secret = get_secret()
+        if not secret:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': 'Failed to get Slack token'})
+            }
+        secrets = json.loads(secret)
+        slack_token = secrets['SLACK_TOKEN']
+        client = WebClient(token=slack_token)
 
-    secret = get_secret()
-    if not secret:
+        message_text = (
+            "*문의 전 채널 상단에 있는 'DX협업툴 가이드'를 꼭 참고해 주세요.*\n"
+            "DX협업툴(Jira, Confluence) 사용 중 단순 문의나 오류 발생 시, 다음의 기술지원 센터를 통해 1차 기술 지원을 활용해 주세요:\n"
+            "<https://sweplateer.atlassian.net/servicedesk/customer/portal/34|기술지원 센터> (SEN Number: SEN-23769245)\n"
+            "관리자 권한이 필요한 업무들은 해당 채널에 남겨주시면 지원하도록 하겠습니다.\n"
+            "(요청사항은 가급적 개인DM, 메일, 전화 연락보다는 채널 활용 부탁드립니다.)\n"
+            "Okta 관련 문의는 <mailto:oktahelper@lguplus.co.kr|oktahelper@lguplus.co.kr>로 문의 주세요."
+        )
+
+        try:
+            response = client.chat_postMessage(channel=channel_id, text=message_text, thread_ts=thread_ts)
+        except SlackApiError as e:
+            print(f"Error sending message: {e}")
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': 'Failed to send message'})
+            }
+
         return {
-            'statusCode': 500,
-            'body': json.dumps({'error': 'Failed to get Slack token'})
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Message sent successfully'})
         }
-    secrets = json.loads(secret)
-    slack_token = secrets['SLACK_TOKEN']
-    client = WebClient(token=slack_token)
-
-    message_text = (
-        "*문의 전 채널 상단에 있는 'DX협업툴 가이드'를 꼭 참고해 주세요.*\n"
-        "DX협업툴(Jira, Confluence) 사용 중 단순 문의나 오류 발생 시, 다음의 기술지원 센터를 통해 1차 기술 지원을 활용해 주세요:\n"
-        "<https://sweplateer.atlassian.net/servicedesk/customer/portal/34|기술지원 센터> (SEN Number: SEN-23769245)\n"
-        "관리자 권한이 필요한 업무들은 해당 채널에 남겨주시면 지원하도록 하겠습니다.\n"
-        "(요청사항은 가급적 개인DM, 메일, 전화 연락보다는 채널 활용 부탁드립니다.)\n"
-        "Okta 관련 문의는 <mailto:oktahelper@lguplus.co.kr|oktahelper@lguplus.co.kr>로 문의 주세요."
-    )
-
-    try:
-        response = client.chat_postMessage(channel=channel_id, text=message_text, thread_ts=thread_ts)
-    except SlackApiError as e:
-        print(f"Error sending message: {e}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': 'Failed to send message'})
-        }
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'message': 'Message sent successfully'})
-    }
