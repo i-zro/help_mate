5ed22017bcc8db939090e03fbb5e11ffbaaf7956
i-zro <naa0@lguplus.co.kr>
2024-04-16 13:39:33 +0900

    lambda error

--- lambda_env/main.py
+++ lambda_env/main.py
@@ -1,11 +1,13 @@
 import json
- 
+
 def lambda_handler(event, context):
     body = json.loads(event['body'])
     
-    # Challenge 요청을 확인하고 응답
     if 'challenge' in body:
         return {
             'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
             'body': json.dumps({'challenge': body['challenge']})
         }
