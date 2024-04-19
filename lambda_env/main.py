81c9cb98024bbef1cb5be499892ed9396e6dae39
권나영/IT최적화팀 <naa0@lguplus.co.kr>
2024-04-19 15:12:54 +0900

    okta test

--- lambda_env/main.py
+++ lambda_env/main.py
@@ -7,7 +7,7 @@ import logging
 
 # 로거 설정
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)  # 로깅 레벨 설정
+logger.setLevel(logging.INFO)
 
 def get_secret():
     secret_name = "slack_bot_token"
@@ -37,6 +37,8 @@ def lambda_handler(event, context):
         }
 
     event_type = body['event'].get('type')
+    channel_id = body['event'].get('channel')
+    message_text = body['event'].get('text', '')
 
     if event_type == 'message_deleted':
         logger.info("Received a message_deleted event, ignoring.")
@@ -54,7 +56,6 @@ def lambda_handler(event, context):
 
     event_ts = body['event'].get('ts')
     thread_ts = body['event'].get('thread_ts', event_ts)
-    channel_id = body['event']['channel']
 
     if thread_ts != event_ts:
         logger.info("Ignoring sub-thread message")
@@ -74,14 +75,19 @@ def lambda_handler(event, context):
     slack_token = secrets['SLACK_TOKEN']
     client = WebClient(token=slack_token)
 
-    message_text = (
-        "*문의 전 채널 상단에 있는 'DX협업툴 가이드'를 꼭 참고해 주세요.*\n"
-        "DX협업툴(Jira, Confluence) 사용 중 단순 문의나 오류 발생 시, 다음의 기술지원 센터를 통해 1차 기술 지원을 활용해 주세요:\n"
-        "<https://sweplateer.atlassian.net/servicedesk/customer/portal/34|기술지원 센터> (SEN Number: SEN-23769245)\n"
-        "관리자 권한이 필요한 업무들은 해당 채널에 남겨주시면 지원하도록 하겠습니다.\n"
-        "(요청사항은 가급적 개인DM, 메일, 전화 연락보다는 채널 활용 부탁드립니다.)\n"
-        "Okta 관련 문의는 <mailto:oktahelper@lguplus.co.kr|oktahelper@lguplus.co.kr>로 문의 주세요."
-    )
+    # 특정 채널 ID 확인 및 "okta" 메시지 반응
+    if channel_id == 'C06DZTAJH0X' and 'okta' in message_text.lower():
+        try:
+            response = client.chat_postMessage(channel=channel_id, text="oktahelper", thread_ts=thread_ts)
+            logger.info("Oktahelper response sent successfully")
+        except SlackApiError as e:
+            logger.error("Error sending oktahelper message: %s", e)
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'error': 'Failed to send oktahelper message'})
+            }
+
+    # 기본 메시지 로직
     try:
         response = client.chat_postMessage(channel=channel_id, text=message_text, thread_ts=thread_ts)
         logger.info("Message sent successfully")
