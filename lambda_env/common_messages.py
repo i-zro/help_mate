2839833c21d5998dc5af8d81b8f2810b83e8d40c
i-zro <naa0@lguplus.co.kr>
2024-07-30 19:48:58 +0900

    confluence, okta 분리

--- lambda_env/common_messages.py
+++ lambda_env/common_messages.py
@@ -2,10 +2,12 @@ DX_TOOL_GUIDE_MESSAGE = (
     "*문의 전 채널 상단에 있는 'DX협업툴 가이드'를 꼭 참고해 주세요.*\n"
     "DX협업툴(Jira, Confluence) 사용 중 단순 문의나 오류 발생 시, 다음의 기술지원 센터를 통해 1차 기술 지원을 활용해 주세요:\n"
     "<https://sweplateer.atlassian.net/servicedesk/customer/portal/34|기술지원 센터> (SEN Number: SEN-23769245)\n"
-    "관리자 권한이 필요한 업무들은 해당 채널에 남겨주시면 지원하도록 하겠습니다.\n"
-    "(요청사항은 가급적 개인DM, 메일, 전화 연락보다는 채널 활용 부탁드립니다.)\n"
-    "Okta 관련 문의는 <mailto:oktahelper@lguplus.co.kr|oktahelper@lguplus.co.kr>로 문의 주세요."
 )
+
+OKTA_MESSAGE = """관리자 권한이 필요한 업무들은 해당 채널에 남겨주시면 지원하도록 하겠습니다.
+(요청사항은 가급적 개인DM, 메일, 전화 연락보다는 채널 활용 부탁드립니다.)
+Okta 관련 문의는 <mailto:oktahelper@lguplus.co.kr|oktahelper@lguplus.co.kr>로 문의 주세요.
+"""
 
 CTO_DIRECT_INVITE = (
     "LG유플러스 CTO워크스페이스는 CTO구성원만 사용이 가능합니다.\n"
--- lambda_env/lambda_handler.py
+++ lambda_env/lambda_handler.py
@@ -62,26 +62,46 @@ def process_slack_event(body):
     event_type = body['event'].get('type')
     channel_id = body['event'].get('channel')
     message_text = body['event'].get('text', '')
-    thread_ts = body['event'].get('thread_ts')  # 스레드의 첫 번째 메시지인지 확인하기 위해 thread_ts를 가져옵니다.
+    thread_ts = body['event'].get('thread_ts')
 
     logger.info(f"Event Type: {event_type}, Channel ID: {channel_id}, thread_ts: {thread_ts}")
 
     # thread_ts가 None인 경우에만 메시지 전송 (스레드의 첫 번째 메시지일 때만 반응)
-    if thread_ts is None:
-        # 특정 단어가 포함된 메시지 필터링 (대소문자 구분 없이)
-        keywords_pattern = re.compile(r'\b(jira|confluence|지라|컨플|옥타|okta)\b', re.IGNORECASE)
-        if keywords_pattern.search(message_text):
-            logger.info("Message contains keywords, sending response")
-            # 테스트 채널용 코드
-            if channel_id in ["C072GNL8A90", "C04CNA12Y6N", "C06DZTAJH0X"]:
-                try:
-                    logger.info(f"Sending DX_TOOL_GUIDE_MESSAGE to channel {channel_id}")
-                    send_slack_message(channel_id, DX_TOOL_GUIDE_MESSAGE, thread_ts) 
-                    return create_response(200, f"Response sent: {DX_TOOL_GUIDE_MESSAGE}")
-                except SlackApiError as e:
-                    logger.error(f"Failed to send message: {e.response['error']}")
-                    return create_response(500, f"Failed to send message: {e.response['error']}")
+    if thread_ts is not None:
+        logger.info("This message is part of a thread; no action taken.")
+        return create_response(200, 'Message is part of a thread; no action taken.')
+
+    if body['event'].get('bot_id'):
+        logger.info("This is a bot message, ignoring.")
+        return create_response(200, 'Ignored bot message')
+
+    # 특정 단어가 포함된 메시지 필터링 (대소문자 구분 없이)
+    keywords_pattern = re.compile(r'\b(jira|confluence|지라|컨플|옥타|okta)\b', re.IGNORECASE)
+    if keywords_pattern.search(message_text):
+        logger.info("Message contains keywords, sending response")
+        # 테스트 채널용 코드
+        if channel_id in ["C072GNL8A90", "C04CNA12Y6N", "C06DZTAJH0X"]:
+            try:
+                logger.info(f"Sending DX_TOOL_GUIDE_MESSAGE to channel {channel_id}")
+                send_slack_message(channel_id, DX_TOOL_GUIDE_MESSAGE, body['event']['ts']) 
+                return create_response(200, f"Response sent: {DX_TOOL_GUIDE_MESSAGE}")
+            except SlackApiError as e:
+                logger.error(f"Failed to send message: {e.response['error']}")
+                return create_response(500, f"Failed to send message: {e.response['error']}")
     
+    okta_pattern = re.compile(r'\b(|옥타|okta)\b', re.IGNORECASE)
+    if okta_pattern.search(message_text):
+        logger.info("Message contains keywords, sending response")
+        # 테스트 채널용 코드
+        if channel_id in ["C072GNL8A90", "C04CNA12Y6N", "C06DZTAJH0X"]:
+            try:
+                logger.info(f"Sending DX_TOOL_GUIDE_MESSAGE to channel {channel_id}")
+                send_slack_message(channel_id, OKTA_MESSAGE, body['event']['ts']) 
+                return create_response(200, f"Response sent: {OKTA_MESSAGE}")
+            except SlackApiError as e:
+                logger.error(f"Failed to send message: {e.response['error']}")
+                return create_response(500, f"Failed to send message: {e.response['error']}")
+            
     return create_response(200, 'No action required')
 
 def create_response(status_code, message):
