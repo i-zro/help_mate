f5eb4030df0f6ce74d2d7e9984b2862a1a262c77
i-zro <naa0@lguplus.co.kr>
2024-07-30 19:09:44 +0900

    common_messages.py

--- lambda_env/common_messages.py
+++ lambda_env/common_messages.py
@@ -14,3 +14,33 @@ CTO_DIRECT_INVITE = (
     "멀티워크스페이스 신청 가이드 - https://lgu-cto.atlassian.net/wiki/spaces/DXFAQ/pages/37951507483\n"
     "Slack Connect 가이드 - https://lgu-cto.atlassian.net/wiki/spaces/DXFAQ/pages/37460836953/Slack+Connect)"
 )
+
+CTO_NEW_WORKER = """요청하신 Slack 도입 절차 공유드립니다.
+
+정보부합성 검토를 사이버보안기술팀과 협의를 해주셔야 하고 솔루션 라이선스 비용에 대해 예산 확보가 필요합니다.
+
+자세한 사항은 아래 내용 확인 부탁드립니다.
+
+[Slack 신규 사용을 위한 도입 절차]
+1. Slack 사용 용도에 대한 IT최적화팀과 협의 진행 (itopt@lguplus.co.kr)
+    > 개발 조직(유플러스 내부 또는 외부 협력사)과 협업하는 조직에 한해 수용함 (ex. 직접개발 조직 및 사업/기획팀, UX 등)
+    > MS Teams/M메신저 대신 Slack을 사용해야 하는 상세 사유
+2. Slack 사용 여부에 대한 사이버보안기술팀과 협의 진행 (secureview@lguplus.co.kr)
+    > Slack 도입 목적 및 Slack 계정 사용 범위 (사용 조직, 협력사 계정 포함 여부, 외부 계정 포함 여부)
+3. Slack 비용에 대한 차년도 사업 예산 확보 및 반영
+    > List Price 기준, 인당 연간 $384, 비용계정명은 지급수수료 일반
+    > 예산 확보/반영 시 4~5월 기준으로 비용 반영 필요
+4. 아래 내용 사이버보안기술팀에게 전달
+    > Slack 사용자 명단
+    > Slack 계정 추가/제거 요청 프로세스
+        ex) 이메일, Slack 채널 또는 Okta Requests를 통해 계정 추가/제거 요청 전달 (입/퇴사자는 마당 연동으로 자동 관리됨)
+5. 아래 내용에 대해 IT최적화팀에 전달
+    > Slack 관리자 명단
+    > Slack 워크스페이스명(LGU+_******)
+        ex) LGU+_CISO, LGU+_NW플랫폼그룹
+    > Slack 도메인명(lgu-***.slack.com)
+        ex) lgu-cto.slack.com, lgu-ciso.slack.com
+6. 지정한 날짜에 Slack 워크스페이스 및 계정 생성 진행 (사이버보안기술팀, IT최적화팀)
+
+참고: LGUplus 엔터프라이즈 내의 다른 워크스페이스에 속하지 않는 멤버를 추가하거나 멀티 채널 게스트로 초대하는 경우에는 정식 멤버와 같은 기준으로 청구됩니다. 예산 확보/반영 시 해당 사항 고려 부탁드립니다. (https://slack.com/intl/ko-kr/help/articles/202518103)
+"""
